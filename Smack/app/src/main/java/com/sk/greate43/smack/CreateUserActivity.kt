f32b9e3881f612a2195ad2e38202ff2d5c2a1901
SALMAN KHAN <greate43.sk@gmail.com>
2018-05-07 20:45:35 +0500

    user avatar beign genearted

--- Smack/app/src/main/java/com/sk/greate43/smack/CreateUserActivity.kt
+++ Smack/app/src/main/java/com/sk/greate43/smack/CreateUserActivity.kt
@@ -1,10 +1,15 @@
 package com.sk.greate43.smack
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import kotlinx.android.synthetic.main.activity_create_user.*
+import java.util.*
 
 class CreateUserActivity : AppCompatActivity() {
+    var userAvatar = "profileDefault"
+    var avatarColor = "[0.5 ,0.5 ,0.5]"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -12,13 +17,34 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun generateUserAvatar(view: View) {
+        val random = Random()
+        val color = random.nextInt(2)
+        val avatar = random.nextInt(28)
+        if (color == 0) {
+            userAvatar = "light$avatar"
+        } else {
+            userAvatar = "dark$avatar"
+        }
+        val resourceId = resources.getIdentifier(userAvatar, "drawable", packageName)
+        createUserAvatarImageView.setImageResource(resourceId)
+    }
+
+    fun generateColorClicked(view: View) {
+        val random = Random()
+        val r = random.nextInt(255)
+        val g = random.nextInt(255)
+        val b = random.nextInt(255)
+        createUserAvatarImageView.setBackgroundColor(Color.rgb(r, g, b))
+
+        val savedR = r.toDouble() / 255;
+        val savedG = g.toDouble() / 255;
+        val savedB = b.toDouble() / 255;
+
+        avatarColor = "[$savedR,$savedG,$savedB,1]"
 
     }
 
-    fun generateColorClicked(view:View){
+    fun createUserClicked(view: View) {
 
     }
-   fun createUserClicked(view:View){
-
-   }
 }
